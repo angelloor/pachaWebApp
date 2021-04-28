f7ec96d921537379961b3a1b153daaebb7ab0de3
ANGEL MIGUEL LOOR MANZANO <miguelangelloor96@gmail.com>
2021-04-28 00:49:44 -0500

    pequeños cambios

--- src/components/User/ItemUser.jsx
+++ src/components/User/ItemUser.jsx
@@ -2,7 +2,19 @@ import React from 'react';
 
 const ItemUser = props => {
     return (
-        <div>
+        <div className="containerItem">
+            <div>
+                <h4>Angel Loor</h4>
+            </div>
+            <div>
+                <h4>Nivel 10</h4>
+            </div>
+            <div>
+                <h4>600</h4>
+            </div>
+            <div>
+                <input type="submit" />
+            </div>
 
         </div>
     );
